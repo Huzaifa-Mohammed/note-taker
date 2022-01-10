2a66d36c3808a9b9ea18315bae4317555fac5677
Huzaifa Mohammed <huzirizwan@gmail.com>
2022-01-09 21:18:26 -0500

    api routes

--- server.js
+++ server.js
@@ -1,5 +1,55 @@
 const express = require('express');
 const app = express();
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+const path = require("path")
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/Develop/public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+
+    res.sendFile(__dirname + "/Develop/public/index.html")
+})
+app.get("/notes", (req, res) => {
+
+    res.sendFile(__dirname + "/Develop/public/notes.html")
+})
+
+
+
+
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(__dirname + "/Develop/db/db.json")
+})
+app.post("/api/notes", function(req, res) {
+    fs.readFile("/Develop/db/db.json", (err, data) => {
+        if (err) throw err;
+
+        let input = JSON.parse(data);
+
+        let newInput = req.body;
+        let uID = (input.length).toString();
+        newInput.id = uId;
+
+        input.push(newInput);
+
+        fs.writeFile("/Develop/db/db.json", JSON.stringify(input), (err, data) => {
+            if (err) throw err;
+            console.log("New note added ");
+        });
+
+        res.json(input);
+    });
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`APP is on port! ` + PORT);
 });
